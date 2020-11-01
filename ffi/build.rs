cbe09f47ec8a669c35066c653310a82cba865870
Danny <danny.de.jong@protonmail.com>
2020-11-01 16:28:11 +0100

    Changed ffi build file.

--- ffi/build.rs
+++ ffi/build.rs
@@ -10,47 +10,28 @@ fn main() {
     let target = env::var("TARGET").unwrap();
 
 	let mut std_flag = "-std=c11";
-	
+
 	if target.contains("windows") {
+		std_flag = "/std:c++17";
+
+		// Win32 source files
 		build
 			.file("src/win32.c")
 			.file("src/application/win32.c")
 			.file("src/window/common.c")
 			.file("src/window/win32.c")
-			.flag("/D")
-			.flag("BW_WIN32");
-	}
+			.flag("/DBW_WIN32");
 
-	// Note: not actually supported atm
-	if cfg!(feature = "webview2") {
-		if target.contains("windows") {
-			std_flag = "/std:c++17";
-
-			build
-				.file("src/browser_window/webview2.cpp")
-				.flag("/D")
-				.flag("BW_WEBVIEW2");
-		}
-	}
-	else {	// Use CEF if no other engine is selected
-		std_flag = "/std:c++17";
-
+		// CEF source files
 		build
 			.file("src/application/cef.cpp")
 			.file("src/browser_window/cef.cpp")
 			.file("src/cef/bw_handle_map.cpp")
 			.file("src/cef/exception.cpp")
-			.flag("/D")
-			.flag("BW_CEF");
-
-		if target.contains("windows") {
-			build
-				.flag("/D")
-				.flag("BW_CEF_WINDOWS");
-				//.flag("/MT");
-		}
+			.flag("/DBW_CEF");
 	}
 
+	// Common source files
 	build
 		.file("src/string.c")
 		.file("src/browser_window/common.c")
