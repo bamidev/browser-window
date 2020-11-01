8d1cf72fd1cc7dd573e21d0ba53c1c4d424d0c35
Danny <danny.de.jong@protonmail.com>
2020-11-01 13:58:36 +0100

    Added a debug header file for putting testing print statements in between code easily.

---
+++ ffi/src/debug.h
@@ -0,0 +1,38 @@
+/*	This file contains all functionality to make tool-less debugging a little bit easier.
+ *	Most macros are simply shorter ways to print stuff to the console, that only get compiled in debug builds.
+ */
+#ifndef BW_DEBUG_H
+#define BW_DEBUG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#include <stdio.h>
+
+
+
+// Print MESSAGE to the standard error output.
+// Additional information can be provided into the message, just like printf.
+#ifndef NDEBUG
+#define BW_DEBUG( MESSAGE, ... ) \
+{ \
+	fprintf( stderr, "[DEBUG %s:%d] ", __FILE__, __LINE__ ); \
+	fprintf( stderr, MESSAGE, __VA_ARGS__ ); \
+	fprintf( stderr, "\n" ); \
+}
+#else
+#define BW_DEBUG
+#endif
+
+
+
+#ifdef __cplusplus
+} //extern "C"
+#endif
+
+
+
+#endif//BW_DEBUG_H
