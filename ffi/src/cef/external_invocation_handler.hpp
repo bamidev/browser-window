15c7c6ca8f085038e3100eb35a184c3a252c6bf9
Danny <danny.de.jong@protonmail.com>
2020-11-06 18:18:27 +0100

    Finished implementing string conversion for external invocation handler.

--- ffi/src/cef/external_invocation_handler.hpp
+++ ffi/src/cef/external_invocation_handler.hpp
@@ -46,29 +46,69 @@ namespace bw {
 		}
 
 	protected:
+		CefString boolToString( bool boolean ) {
+			if ( boolean )
+				return "true";
+			// else
+			return "false";
+		}
+
+		template <class V>
+		CefString intoString( const V& value ) {
+			std::string str = std::to_string( value );
+			return CefString( str );
+		}
+
 		// Convert a javascript value into a string, for very basic compatibility purposes with the Rust application
 		// Note: This function is not yet complete. Not all types are converted appropriately.
 		CefString v8ValueToString( CefRefPtr<CefV8Value> val ) {
+
+			// If undefined
+			if ( val->IsUndefined() )
+				return "undefined";
+
+			// If null
+			if ( val->IsNull() )
+				return "null";
 
 			// If string
 			if ( val->IsString() )
 				return val->GetStringValue();
 
 			// If boolean
-			if ( val->IsBool() ) {
+			if ( val->IsBool() )
+ 				return this->boolToString( val->GetBoolValue() );
 
-				if ( val->GetBoolValue() ) {
-					return "true";
-				}
-				else {
-					return "false";
-				}
-			}
+			// If integer
+			if ( val->IsInt() )
+				return this->intoString( val->GetIntValue() );
 
-			// TODO: Convert all other possible javascript types to string as well
+			// If unsigned integer
+			if ( val->IsUInt() )
+				return this->intoString( val->GetUIntValue() );
+
+			// If unsigned integer
+			if ( val->IsDouble() )
+				return this->intoString( val->GetDoubleValue() );
+
+			// If object (unsupported)
+			if ( val->IsObject() )
+				return "[object]";
+
+			// If array (unsupported)
+			if ( val->IsArray() )
+				return "[array]";
+
+				// If array (unsupported)
+			if ( val->IsDate() )
+				return "[date]";
+
+			// If exception (unsupported)
+			if ( val->IsFunction() )
+				return "[function]";
 
 			// If type is not accounted for, return this string:
-			return "[type not supported yet]";
+			return "[unknown type]";
 		}
 
 		IMPLEMENT_REFCOUNTING(ExternalInvocationHandler);
