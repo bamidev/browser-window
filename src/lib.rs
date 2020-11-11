f4183262595649a0f39ac839c341f4fb8e2bd7f9
Danny <danny.de.jong@protonmail.com>
2020-11-11 01:33:21 +0100

    Updated simple example from the main documentation page.

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,29 @@
 //! Browser Window is a Rust crate that allows you to have and manipulate windows with browsers in them.
 //! Just like Electron, you can build graphical user interfaces with HTML/CSS/JS technology, but you can also use it to just have some browser functionality in your application.
 //!
-//! To start using Browser Window, you need to start it before anything else, and preferably on the main thread.
-//! To do this we use `Application::start()`, which gives you an application handle that can be used to create the browser windows.
+//! To start using Browser Window, you need to start it (before anything else) and run it, and preferably on the main thread.
 //!
 //! Your program might look something like this:
 //! ```
 //! use browser_window::*;
+//! use std::process::exit;
 //!
 //! fn main() {
-//! 	let app = Application::start();
+//! 	let runtime = Runtime::start();
 //!
-//! 	BrowserWindowBuilder::new( Source::Url("https://www.duckduckgo.com/".to_owned()) )
-//! 	.spawn( &app, |browser| {
-//! 		browser.exec_js(" ... ");
+//! 	let exit_code = runtime.run(|app| {
+//!
+//! 		BrowserWindowBuilder::new( Source::Url("https://www.duckduckgo.com/".into()) )
+//! 			.spawn( &app, |browser| {
+//! 				browser.exec_js("document.getElementById('search_form_input_homepage').value = 'Hello World!'");
+//! 			});
 //! 	});
+//!
+//! 	exit( exit_code );
 //! }
 //! ```
 //!
 //! For an example that uses Browser Window in an asynchronous context, see [this example code](https://github.com/bamilab/browser-window/blob/master/example/src/main.rs).
-
-#![cfg_attr(feature = "nightly", feature(negative_impls))]
 
 mod application;
 mod browser;
