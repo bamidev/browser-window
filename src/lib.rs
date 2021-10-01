74a304b3da493b0653e180f18c3c254cee6e516a
Danny de Jong <danny.de.jong@protonmail.com>
2021-10-01 14:01:24 +0200

    Updated main documentation page.

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,16 @@
 //! The API is bound to have breaking changes every minor version increment.
 //! Once version 1.0.0 is released, minor version increments will be backwards-compatible.
 //!
-//! # Warning
-//! At the moment only Windows is suppored, but Linux support is on the way.
-//!
 //! Moreover, the only supported browser engine right now is Chromium's Blink.
 //! Therefore, the Chromium Embedding Framework (CEF) is used as a dependency.
 //! To get CEF set up properly, enable feature `cef`, and take a look [here](https://github.com/bamilab/browser-window/tree/master/docs/getting-started).
 //! 
+//! # Warning
+//! Due to insufficient support of GTK by the browser embedding API, some windowing features are only available on Windows.
+//! 
 //! # Getting Started
 //! To start building apps with Browser Window, take a quick look at the [`application`](application/index.html) module.
-//!
+//! 
 //! For a rich example, see [this example code](https://github.com/bamilab/browser-window/blob/master/example/).
 
 
