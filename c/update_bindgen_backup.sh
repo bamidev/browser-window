3c9d28a2f90a8c1bc2cae4379ffb5cb0e9d98cc6
Danny de Jong <danny.de.jong@protonmail.com>
2021-10-03 10:01:39 +0200

    Added comments.

--- c/update_bindgen_backup.sh
+++ c/update_bindgen_backup.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+#
+# This invokes the build script in a way that regenerates the bindgen file and places it in ./bindgen_backup.rs
+# This file will be then be present in docs.rs' build environment.
+
 env UPDATE_BINDGEN_BACKUP=1 cargo build
