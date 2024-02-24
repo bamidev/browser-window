20a436e5f2f2c29c90271e22dd4c94b4b4c06396
Bamidev <bamidev@pm.me>
2024-02-24 12:25:25 +0100

    Notify the user that they need to specify a feature.

---
+++ build.rs
@@ -0,0 +1,8 @@
+use std::env;
+
+
+fn main() {
+    if env::var("DOCS_RS").is_err() && !cfg!(feature = "cef") && !cfg!(feature = "webkitgtk") {
+        panic!("No browser framework has been specified. Enable either feature `webkitgtk` or `cef`.");
+    }
+}
