81ef6a63dae1ea5ded0defe9ba4309f6e6a98444
Bamidev <bamidev@pm.me>
2024-03-01 12:32:39 +0100

    Improved cef copy script.

--- setup-cef-files.sh
+++ setup-cef-files.sh
@@ -6,6 +6,8 @@ mkdir -p ./target/debug
 cp -r "$CEF_PATH/Resources/"* ./target/debug
 cp -r "$CEF_PATH/Release/"* ./target/debug
 
-echo The script needs root permissions to change permissions. >&2
+if [ "$EUID" -ne 0 ]; then
+	echo The script needs root permissions to change permissions. >&2
+fi
 sudo chown root:root ./target/debug/chrome-sandbox
 sudo chmod 4755 ./target/debug/chrome-sandbox
