cde55b7bcfc49c2a63ee7861b39fbcbce15cd9fd
Bamidev <bamidev@pm.me>
2024-03-02 23:08:56 +0100

    Fix the cef setup scripts.

--- setup-cef-files-tests.sh
+++ setup-cef-files-tests.sh
@@ -3,6 +3,5 @@
 cp -r "$CEF_PATH/Resources/"* .
 cp -r "$CEF_PATH/Release/"* .
 
-echo The script needs root permissions to change permissions. >&2
 sudo chown root:root ./chrome-sandbox
 sudo chmod 4755 ./chrome-sandbox
--- setup-cef-files.sh
+++ setup-cef-files.sh
@@ -6,8 +6,5 @@ mkdir -p ./target/debug
 cp -r "$CEF_PATH/Resources/"* ./target/debug
 cp -r "$CEF_PATH/Release/"* ./target/debug
 
-if [ "$EUID" -ne 0 ]; then
-	echo The script needs root permissions to change permissions. >&2
-fi
-sudo chown root:root ./target/debug/chrome-sandbox
-sudo chmod 4755 ./target/debug/chrome-sandbox
+chown root:root ./target/debug/chrome-sandbox
+chmod 4755 ./target/debug/chrome-sandbox
