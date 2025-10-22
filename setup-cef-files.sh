a262e326f487e18084425913c4c0fd74574c1b68
Bamidev <bamidev@pm.me>
2025-10-22 22:07:15 +0200

    Fix github ci for CEF in release mode

--- setup-cef-files.sh
+++ setup-cef-files.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 mkdir -p ./target/debug
+mkdir -p ./target/release
 
 cp -r "$CEF_PATH/Resources/"* ./target/debug
 cp -r "$CEF_PATH/Release/"* ./target/debug
+cp -r "$CEF_PATH/Resources/"* ./target/release
+cp -r "$CEF_PATH/Release/"* ./target/release
 
 #chown root:root ./target/debug/chrome-sandbox
 #chmod 4755 ./target/debug/chrome-sandbox
