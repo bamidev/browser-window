3611cf9c32166eb81e07db11b0dd538a86c3f638
Bamidev <bamidev@pm.me>
2025-10-24 20:44:13 +0200

    Also test setup-cef-files.bat in github ci

--- setup-cef-files.bat
+++ setup-cef-files.bat
@@ -1,4 +1,7 @@
 mkdir target\debug
+mkdir target\release
 
 xcopy /Y %CEF_PATH%\Resources\* target\debug
+xcopy /Y %CEF_PATH%\Resources\* target\release
 xcopy /Y %CEF_PATH%\Release\* target\debug
+xcopy /Y %CEF_PATH%\Release\* target\release
