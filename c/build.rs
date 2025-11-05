bb13a95e1af3dfb4c4fa88d258a2d21bf10f7645
Bamidev <bamidev@pm.me>
2025-11-05 20:16:04 +0100

    Test if cef can be made to work on macos

--- c/build.rs
+++ c/build.rs
@@ -140,6 +140,10 @@ fn main() {
 		build.define("BW_WINDOWS", None);
 		build_se.define("BW_WINDOWS", None);
 	}
+	if target.contains("macos") {
+		build.define("BW_MACOS", None);
+		build_se.define("BW_MACOS", None);
+	}
 	if target.contains("msvc") {
 		build.flag("/MT");
 	}
@@ -215,68 +219,73 @@ fn main() {
 				build_se.include(&cef_path);
 
 				// Link with CEF
-				println!("cargo:rustc-link-search={}/libcef_dll_wrapper", &cef_path);
-				println!("cargo:rustc-link-search={}/Release", &cef_path);
-				if target.contains("msvc") {
-					build_se.flag("/MT");
+                println!("cargo:rustc-link-search={}/libcef_dll_wrapper", &cef_path);
+                if target.contains("apple-darwin") {
+                    println!("cargo:rustc-link-search=framework={}/Release", &cef_path);
+                    println!("cargo:rustc-link-lib=framework=Chromium Embedded Framework");
+                    build_se_lib_args.push(format!("-F{}/Release", &cef_path).into());
+                    //build_se_lib_args.push("-framework".into());
+                    //build_se_lib_args.push("Chromium Embedded Framework".into());
+                    build_se_lib_args.push(format!("-L{}/libcef_dll_wrapper", &cef_path).into());
+                } else {
+                    println!("cargo:rustc-link-search={}/Release", &cef_path);
+                    if target.contains("msvc") {
+                        build_se.flag("/MT");
 
-					println!("cargo:rustc-link-search={}", &cef_path);
-					println!(
-						"cargo:rustc-link-search={}/libcef_dll_wrapper/Release",
-						&cef_path
-					);
-					println!("cargo:rustc-link-lib=static=libcef_dll_wrapper");
-					println!("cargo:rustc-link-lib=static=libcef");
-					println!("cargo:rustc-link-lib=dylib=libcef");
+                        println!("cargo:rustc-link-search={}", &cef_path);
+                        println!(
+                            "cargo:rustc-link-search={}/libcef_dll_wrapper/Release",
+                            &cef_path
+                        );
+                        println!("cargo:rustc-link-lib=static=libcef_dll_wrapper");
+                        println!("cargo:rustc-link-lib=static=libcef");
+                        println!("cargo:rustc-link-lib=dylib=libcef");
 
-					build_se_lib_args.push(format!("/LIBPATH:{}", &cef_path).into());
-					build_se_lib_args
-						.push(format!("/LIBPATH:{}/libcef_dll_wrapper", &cef_path).into());
-					build_se_lib_args
-						.push(format!("/LIBPATH:{}/libcef_dll_wrapper/Release", &cef_path).into());
-					build_se_lib_args.push(format!("/LIBPATH:{}/Release", &cef_path).into());
-				} else {
-					println!("cargo:rustc-link-lib=static=cef_dll_wrapper");
-					println!("cargo:rustc-link-lib=static=cef");
-					println!("cargo:rustc-link-lib=dylib=cef");
+                        build_se_lib_args.push(format!("/LIBPATH:{}", &cef_path).into());
+                        build_se_lib_args
+                            .push(format!("/LIBPATH:{}/libcef_dll_wrapper", &cef_path).into());
+                        build_se_lib_args
+                            .push(format!("/LIBPATH:{}/libcef_dll_wrapper/Release", &cef_path).into());
+                        build_se_lib_args.push(format!("/LIBPATH:{}/Release", &cef_path).into());
+                    } else {
+                        println!("cargo:rustc-link-lib=static=cef_dll_wrapper");
+                        println!("cargo:rustc-link-lib=static=cef");
+                        println!("cargo:rustc-link-lib=dylib=cef");
 
-					build_se_lib_args.push(format!("-L{}/libcef_dll_wrapper", &cef_path).into());
-					build_se_lib_args.push(format!("-L{}/Release", &cef_path).into());
+                        build_se_lib_args.push(format!("-L{}/libcef_dll_wrapper", &cef_path).into());
+                        build_se_lib_args.push(format!("-L{}/Release", &cef_path).into());
 
-                    #[cfg(not(target_os = "macos"))]
-                    let pkg_deps = &[
-                        "alsa",
-                        "cups",
-                        "dbus-1",
-                        "expat",
-                        "gtk+-3.0",
-                        "libdrm",
-                        "nss",
-                        "gbm",
-                        "x11",
-                        "x11-xcb",
-                        "xcomposite",
-                        "xdamage",
-                        "xext",
-                        "xfixes",
-                        "xkbcommon",
-                        "xrandr",
-                    ];
-                    #[cfg(target_os = "macos")]
-                    let pkg_deps: &[&'static str] = &[];
-                    for pkg_name in pkg_deps {
-                        let result = pkg_config::Config::new().probe(pkg_name).expect(&format!("Unable to find {}", pkg_name));
+                        let pkg_deps = &[
+                            "alsa",
+                            "cups",
+                            "dbus-1",
+                            "expat",
+                            "gtk+-3.0",
+                            "libdrm",
+                            "nss",
+                            "gbm",
+                            "x11",
+                            "x11-xcb",
+                            "xcomposite",
+                            "xdamage",
+                            "xext",
+                            "xfixes",
+                            "xkbcommon",
+                            "xrandr",
+                        ];
+                        for pkg_name in pkg_deps {
+                            let result = pkg_config::Config::new().probe(pkg_name).expect(&format!("Unable to find {}", pkg_name));
 
-                        if *pkg_name == "x11" {
-                            // When X11 is used, browser-window-c uses a bit of x11 code as well
-                            for inc in &result.include_paths {
-                              build.include(inc);
-                              build_se.include(inc);
+                            if *pkg_name == "x11" {
+                                // When X11 is used, browser-window-c uses a bit of x11 code as well
+                                for inc in &result.include_paths {
+                                  build.include(inc);
+                                  build_se.include(inc);
+                                }
                             }
                         }
                     }
-				}
-
+                }
 			}
 		}
 
